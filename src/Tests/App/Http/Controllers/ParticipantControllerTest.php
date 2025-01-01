71143d8569f3eef71580bb801e7ecab5398eaf21
Rodolfo Alejandro Oquendo Rojas <65622792+rodolfoquendo@users.noreply.github.com>
2025-01-01 19:13:06 -0300

    updates

--- src/Tests/App/Http/Controllers/ParticipantControllerTest.php
+++ src/Tests/App/Http/Controllers/ParticipantControllerTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\App\Http\Controllers;
 
+use App\Models\Participant;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Config;
 
@@ -11,89 +12,30 @@ class ParticipantControllerTest extends \Tests\TestCase
     /**
      * A basic test example.
      */
-    public function test__get__no_auth(): void
-    {
-        $this->createApplication();
-        $response = $this->getJson($this->APIRoute_v1('tournament/participants'));
-        $response->assertUnauthorized();
-        $response = $this->getJson($this->APIRoute_v1('tournament/participants?name=Rodolfo+Oquendo'));
-        $response = $response->json();
-        $response->assertTrue(isset($response['name']));
-        $response->assertTrue($response['name'] == 'Rodolfo Oquendo');
-
-    }
-
-    /**
-     * A basic test example.
-     */
     public function test__get(): void
     {
         $this->createApplication();
-
-    }
-
-    /**
-     * A basic test example.
-     */
-    public function test__login__validationFailure(): void
-    {
-        $this->createApplication();
-        $response = $this->postJson($this->APIRoute_v1('auth/login'),[
-            'email' => 'asd',
-            'password' => 'asd',
-        ]);
-        $response->assertUnprocessable();
-    }
-
-    /**
-     * A basic test example.
-     */
-    public function test__login__wrongData(): void
-    {
-        $this->createApplication();
-        $response = $this->postJson($this->APIRoute_v1('auth/login'),[
-            'email' => 'asdd@asd.asd',
-            'password' => 'asdd',
-        ],[
-            'Content-Type' => 'application/json',
-        ]);
+        $response = $this->getJson($this->APIRoute_v1('participants'));
+        $response->assertUnauthorized();
+        $response = $this->getJson($this->APIRoute_v1('participants?name=' . \urlencode(str_repeat('a',300))), $this->limitlessHeader());
         $response->assertUnprocessable();
 
-    }
-    /**
-     * A basic test example.
-     */
-    public function test__login__exception(): void
-    {
-        $this->createApplication();
-        Config::set('database.connections.mysql.port', '3333');
-        $response = $this->postJson($this->APIRoute_v1('auth/login'),[
-            'email' => env('MASTER_USER_EMAIL','rodolfoquendo@gmail.com'),
-            'password' => env('MASTER_USER_PASSWORD','12345678'),
-        ]);
-        $this->assertTrue(isset($response['success']));
-        $this->assertFalse($response['success']);
-        $this->assertTrue(isset($response['status']));
-        Config::set('database.connections.mysql.port', '3306');
-    }
 
-    /**
-     * A basic test example.
-     */
-    public function test__login__success(): void
-    {
-        $this->createApplication();
-        $response = $this->postJson($this->APIRoute_v1('auth/login'),[
-            'email' => env('MASTER_USER_EMAIL','rodolfoquendo@gmail.com'),
-            'password' => env('MASTER_USER_PASSWORD','12345678'),
-        ]);
+        $participant = Participant::find(1);
+        $response = $this->getJson($this->APIRoute_v1('participants?name=' . \urlencode($participant->name)), $this->limitlessHeader());
         $response = $response->json();
-        $this->assertTrue(isset($response['success']));
-        $this->assertTrue(isset($response['status']));
         $this->assertTrue(isset($response['payload']));
-        $this->assertTrue($response['success']);
-        $this->assertTrue($response['status'] == Response::HTTP_OK);
-        $this->assertTrue(isset($response['payload']['access_token']));
+        $this->assertTrue(isset($response['payload']['name']));
+        $this->assertTrue($response['payload']['name'] == $participant->name);
+        $response = $this->getJson($this->APIRoute_v1('participants?name=asd'), $this->limitlessHeader());
+        $response = $response->json();
+        $this->assertNull($response['payload']);
+
+        Config::set('database.connections.mysql.port', '3333');
+        $response = $this->getJson($this->APIRoute_v1('participants?name=asd'), $this->limitlessHeader());
+        
+        Config::set('database.connections.mysql.port', '3306');
 
     }
+
 }
